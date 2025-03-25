ce672a0d50024fe74f42853ac9c2d489b27d2464
SushiRenz <renz09358@gmail.com>
2025-03-25 20:53:44 +0800

    Final final final

---
+++ PetNameGenerator/PetNameGenerator/Constants/Name.cs
@@ -0,0 +1,27 @@
+﻿namespace PetNameGenerator.Controllers.Constants
+{
+    public static class Names
+    {
+        public static class Dog
+        {
+            public static readonly string[] First = new[]
+            { "Cloud", "Zack", "Cid", "Barrett", "Red XIII", "Nanaki", "Dirge", "Angeal", "Genesis", "Sephiroth" };
+            public static readonly string[] Last = new[]
+            { "Strife", "Fair", "Highwind", "Wallace", "Lockhart", "Valentine", "Shinra", "Hewley", "Rhapsodos", "Leonhart" };
+        }
+        public static class Cat
+        {
+            public static readonly string[] First = new[]
+            { "Tifa", "Aerith", "Yuna", "Rikku", "Lightning", "Serah", "Fran", "Terra", "Celes", "Garnet" };
+            public static readonly string[] Last = new[]
+            { "Lockhart", "Gainsborough", "Besaid", "Al Bhed", "Farron", "Villiers", "Viera", "Branford", "Chere", "Tribal" };
+        }
+        public static class Bird
+        {
+            public static readonly string[] First = new[]
+            { "Chocobo", "Ifrit", "Shiva", "Ramuh", "Bahamut", "Odin", "Phoenix", "Sylph", "Carbuncle", "Alexander" };
+            public static readonly string[] Last = new[]
+            { "Rider", "Esper", "Guardian", "Summon", "King", "Knight", "Warrior", "Spirit", "Protector", "Eidolon" };
+        }
+    }
+}
