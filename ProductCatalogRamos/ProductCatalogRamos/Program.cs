0dda7439788c2480791f09ad971a0aeae5702ed3
SushiRenz <renz09358@gmail.com>
2025-04-16 15:37:51 +0800

    done API (still no UI)

--- ProductCatalogRamos/ProductCatalogRamos/Program.cs
+++ ProductCatalogRamos/ProductCatalogRamos/Program.cs
@@ -1,4 +1,8 @@
+using ProductCatalogService.ProductModels;
+using ProductCatalogServiceRamos.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<ProductsDatabaseSettings>(
+    builder.Configuration.GetSection("ProductsDatabase"));
+
+builder.Services.AddSingleton<ProductsService>();
 
 var app = builder.Build();
 
--- ProductCatalogServiceRamos/ProductCatalogServiceRamos/Controllers/ProductConnectServerController.cs
+++ ProductCatalogServiceRamos/ProductCatalogServiceRamos/Controllers/ProductConnectServerController.cs
@@ -1,40 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductCatalogServiceRamos.ProductModels;
+using ProductCatalogServiceRamos.Services;
 
 namespace ProductCatalogServiceRamos.Controllers
 {
     [ApiController]
-    [Route("[Controller")]
-    public class ProductConnectServerController : Controller
+[Route("[controller]")]
+public class ProductConnectServerController : Controller
+{
+    private readonly ProductsService _productsServices;
+
+    public ProductConnectServerController(ProductsService ProductsServices)
     {
-        [HttpGet("/api/products")]
-        public IActionResult GetAllProducts()
-        {
-            return Ok();
-        }
-
-        [HttpGet("/api/products/{id}")]
-        public IActionResult GetProductById(int id)
-        {
-            return Ok();
-        }
-
-        [HttpPost("/api/products")]
-        public IActionResult AddProduct()
-        {
-            return Ok();
-        }
-
-        [HttpPut("/api/products/{id}")]
-        public IActionResult UpdateProduct(int id)
-        {
-            return Ok();
-        }
-
-        [HttpDelete("/api/products/{id}")]
-        public IActionResult DeleteProduct(int id)
-        {
-            return Ok();
-        }
+        _productsServices = ProductsServices;
     }
 
+    [HttpGet]
+    public async Task<List<Products>> Get() =>
+    await _productsServices.GetAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Products>> Get(string id)
+    {
+        var product = await _productsServices.GetAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Products newProducts)
+    {
+        await _productsServices.CreateAsync(newProducts);
+
+        return CreatedAtAction(nameof(Get), new { id = newProducts.Id }, newProducts);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Products updatedProducts)
+    {
+        var product = await _productsServices.GetAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        updatedProducts.Id = product.Id;
+
+        await _productsServices.UpdateAsync(id, updatedProducts);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var product = await _productsServices.GetAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        await _productsServices.RemoveAsync(id);
+
+        return NoContent();
+    }
+
+}
 }
